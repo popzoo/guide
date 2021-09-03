9cd7041098bb50393a65f9d9667724d31d2e78bf
Pop Bob <13653691+popsee@users.noreply.github.com>
2021-09-03 09:00:37 +0800

    Add files via upload

--- js/renewLink.js
+++ js/renewLink.js
@@ -22,14 +22,17 @@
     	document.getElementById("own_form").setAttribute("href",serverUrl+"/form.html");//popzoo.github.io/form/
 	}
     if(document.getElementById("own_data")!=undefined){
-        document.getElementById("own_data").setAttribute("href",serverUrl+"/bi.html");//popzoo.github.io/form/
+        document.getElementById("own_data").setAttribute("href",serverUrl+"/bi.html");//popzoo.github.io/bi/
     }
     if(document.getElementById("own_audio")!=undefined){
         document.getElementById("own_audio").setAttribute("href",serverUrl+"/audio.html");
     }
-    if(document.getElementById("own_cfip")!=undefined){
-        document.getElementById("own_cfip").setAttribute("href",serverUrl+"/cfip.html");
-    }          
+    if(document.getElementById("own_novel")!=undefined){
+        document.getElementById("own_novel").setAttribute("href",serverUrl+"/novel.html");
+    }       
+    // if(document.getElementById("own_cfip")!=undefined){
+    //     document.getElementById("own_cfip").setAttribute("href",serverUrl+"/cfip.html");
+    // }          
     // github link
     if(document.getElementById("left_link")!=undefined){
         document.getElementById("left_link").setAttribute("href","https://ghproxy.com/");
@@ -42,14 +45,14 @@
     //====================================================== 
     window.addEventListener('load', () => {
         console.info('\n' + ' %c Author GitHub ' +  ' %c https://github.com/popzoo ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
-        console.log(`%cPopzoo Page Loaded Time:【${Math.round(performance.now() * 100) / 100}】ms`, 'text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, .1), 0 0 5px rgba(0, 0, 0, .1), 0 1px 3px rgba(0, 0, 0, .3), 0 3px 5px rgba(0, 0, 0, .2), 0 5px 10px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .2), 0 20px 20px rgba(0, 0, 0, .15);\
+        console.info(`%cPopzoo Page Loaded Time:【${Math.round(performance.now() * 100) / 100}】ms`, 'text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, .1), 0 0 5px rgba(0, 0, 0, .1), 0 1px 3px rgba(0, 0, 0, .3), 0 3px 5px rgba(0, 0, 0, .2), 0 5px 10px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .2), 0 20px 20px rgba(0, 0, 0, .15);\
             font-size: 2em;');
     });    
     //  //====================================================== 
     var OriginTitile = document.title, titleTime;
     document.addEventListener('visibilitychange', function() {
         if (document.hidden) {
-            document.title = '道友请留步⊂⊙•⊙⊃';
+            document.title = '小主失联了⊂⊙•⊙⊃';
             clearTimeout(titleTime);
         } else {
             document.title = '带小主回家┗(^_^)┛';
